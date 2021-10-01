4eddf6bfa3be7fa073a0654d673d3da7e729b66d
bunian321 <91712421+bunian321@users.noreply.github.com>
2021-10-01 17:49:31 +0800

    Add files via upload

---
+++ text.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*int main(void)
+{
+	printf("天\n地\n人\n");
+
+	return 0;
+}*/
+/*int main(void)
+{
+	printf("15-37的和是%d。\n",15-37);
+
+	return 0;
+}*/
+/*int main(void)
+{
+	printf("喂！\n\n您好！\n再见。\n");
+
+	return 0;
+}*/
+/*int main(void)
+{
+	int vx = 3.14;
+	printf("vx的值是%d。\n", vx);
+	return 0;
+
+}/*将小数值放入int类型中运行会输出整数部分*/
+/*
+int main(void)
+{
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", & no);   /*返回值被忽略，开头加上 #define _CRT_SECURE_NO_WARNINGS
+	printf("no+12的值为%d。\n", no + 12);
+	return 0;
+}
+*/
+/*
+int main(void)
+{
+	puts("天");
+	puts("地");
+	puts("人");
+	return 0;/*puts在结尾自动换行
+}*/
+/*
+int main(void)
+{
+	int n1, n2, n3;
+	int wa;
+	puts("输入三个整数。");
+	printf("整数1："); scanf("%d", &n1);
+	printf("整数2："); scanf("%d", &n2);
+	printf("整数3："); scanf("%d", &n3);
+	wa = n1 + n2 + n3;
+	printf("它们的和是%d。\n", wa);
+	return 0;
+}*/
